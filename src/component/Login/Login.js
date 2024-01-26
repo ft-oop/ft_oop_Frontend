31b9b7a1985bd721f504f6b179e4aa16d96667c6
oio337a <oio337a@gmail.com>
2024-01-26 23:25:55 +0900

    chore : 파일 위치변경

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,4 +1,34 @@
-const root = document.getElementById('root');
-
-
-
+export default function Login() {
+  return /*html*/ `
+  <div class="flex flex-col justify-center items-center">
+    <img
+      src="/logo.png"
+      alt="logo"
+      class="pointer-events-none w-[500px] h-[500px]"
+    />
+    <div
+      class="flex justify-center items-center text-2xl font-semibold hover:text-3xl hover:font-bold text-purple-400 w-[200px] h-[200px] bg-cover cursor-pointer"
+      style="background-image: url(/bubble.png)"
+    >
+      Login
+    </div>
+    <div class="-z-10">
+      <img
+        src="/bubble.png"
+        alt="img"
+        class="pointer-events-none absolute -bottom-40 -left-40 w-[500px] h-[500px]"
+      />
+      <img
+        src="/bubble.png"
+        alt="img"
+        class="pointer-events-none absolute -bottom-28 -right-40 w-[600px] h-[600px]"
+      />
+      <img
+        src="/bubble.png"
+        alt="img"
+        class="pointer-events-none absolute bottom-80 -right-10 w-[200px] h-[200px]"
+      />
+    </div>
+  </div>
+  `;
+}
