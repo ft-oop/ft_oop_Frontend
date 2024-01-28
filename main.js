e402a6b45d99321c19c03a1c0aa465b1e72819bb
zinnnn37 <zinnnn37@gmail.com>
2024-01-29 08:28:07 +0900

    chore : SPA test

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-import Login from './src/component/Login/Login.js';
+// import Login from './src/component/Login/Login.js';
+import MyPage from './src/component/MyPage/MyPage.js';
 
-document.querySelector('#app').innerHTML = Login();
+// document.querySelector('#app').innerHTML = Login();
+document.querySelector('#app').innerHTML = MyPage();
