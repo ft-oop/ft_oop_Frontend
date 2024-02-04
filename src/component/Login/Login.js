5e7fa26eb4daff4ccce9575a3700f65232cc9d2c
oio337a <oio337a@gmail.com>
2024-02-04 13:06:34 +0900

    refactor : class 화로 변경

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,17 +1,30 @@
-export default function Login() {
-  return /*html*/ `
-  <div class="flex flex-col justify-center items-center">
-    <img
-      src="/logo.png"
-      alt="logo"
-      class="pointer-events-none w-[500px] h-[500px]"
-    />
-    <div
-      class="flex justify-center items-center text-2xl font-semibold hover:text-3xl hover:font-bold text-purple-400 w-[200px] h-[200px] bg-cover cursor-pointer"
-      style="background-image: url(/bubble.png)"
-    >
-      Login
-    </div>
+import { $ } from "../../utils/querySelector.js"
+import { BASE_URL} from '../../constant/routeInfo.js';
+import { navigate } from '../../utils/navigate.js';
+
+function Login($container) {
+  this.$container = $container;
+
+  this.setState = () => {
+    this.render();
+  }
+
+
+  this.render = () => {
+    this.$container.innerHTML = `
+    <div class="flex flex-col justify-center items-center">
+      <img
+        src="/logo.png"
+        alt="logo"
+        class="pointer-events-none w-[500px] h-[500px]"
+      />
+      <div
+        id="loginBtn"
+        class="flex justify-center items-center text-2xl font-semibold hover:text-3xl hover:font-bold text-purple-400 w-[200px] h-[200px] bg-cover cursor-pointer"
+        style="background-image: url(/bubble.png)"
+      >
+        Login
+      </div>
     <div class="-z-10">
       <img
         src="/bubble.png"
@@ -30,5 +43,18 @@ export default function Login() {
       />
     </div>
   </div>
-  `;
+    `;
+  }
+  this.init = () => {
+    this.render();
+    $('#loginBtn').addEventListener('click', (e) => {
+      const targetURL = window.location.href.replace(BASE_URL, "login");
+      navigate(targetURL);
+    });
+  }
+
+
+  this.init();
 }
+
+export  default Login;
