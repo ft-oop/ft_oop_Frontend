dcc0ccd3a73df52c04841c992322d1a2276bd630
oio337a <oio337a@gmail.com>
2024-02-04 14:48:47 +0900

    refactor : template 분리

--- src/component/MyPage/MyPage.js
+++ src/component/MyPage/MyPage.js
@@ -49,29 +49,6 @@ export default class MyPage extends Component {
     };
   }
 
-  template() {
-    return `
-      <div class="Wrapper">
-      <img id="back" src="/eva--arrow-back-fill.svg" alt="back arrow">
-        <div class="MyPage_container">
-          <div class="MyPage_profile_container">
-            <div class="MyPage_profile"></div>
-          </div>
-          <div class="MyPage_info">
-            <div class="MyPage_info__history"></div>
-            <div class="MyPage_info__user_list"></div>
-          </div>
-        </div>
-        </div>
-      <div id="Background">
-        <img id="bubble1" src="/bubble.png" alt="bubble">
-        <img id="bubble2" src="/bubble.png" alt="bubble">
-        <img id="bubble3" src="/bubble.png" alt="bubble">
-        <img id="bubble4" src="/bubble.png" alt="bubble">
-      </div>
-    `;
-  }
-
   mounted() {
     const $profile = this.$target.querySelector('.MyPage_profile');
     const $historyTable = this.$target.querySelector('.MyPage_info__history');
