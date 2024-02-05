cddb86066a9398f77d73731aa3fb4f9b899d29c9
oio337a <oio337a@gmail.com>
2024-02-05 19:49:54 +0900

    feat : bootstrap props 추가

--- src/utils/bootstrap.js
+++ src/utils/bootstrap.js
@@ -1,8 +1,8 @@
-export const bootstrapInput = (text, placeholder, id) => {
+export const bootstrapInput = (text, placeholder, id, disabled = null) => {
   return `
       <div class="input-group">
         <span class="input-group-text">${text}</span>
-        <input type="text" class="form-control" id="${id}" placeholder="${placeholder}" aria-label="NickName" aria-describedby="input_nickname">
+        <input type="text" class="form-control" id="${id}" ${disabled} placeholder="${placeholder}" aria-label="NickName" aria-describedby="input_nickname">
       </div>
     `;
 };
