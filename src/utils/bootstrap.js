30710acbdd004df03a12764ad83984b5fc53d20a
oio337a <oio337a@gmail.com>
2024-02-19 15:37:49 +0900

    style : text aline, font weight

--- src/utils/bootstrap.js
+++ src/utils/bootstrap.js
@@ -1,7 +1,7 @@
 export const bootstrapInput = (text, placeholder, id, disabled = null) => {
   return `
-      <div class="input-group text-center">
-        <span class="input-group-text w-20">${text}</span>
+      <div class="input-group">
+        <span class="input-group-text w-20 flex justify-center font-medium">${text}</span>
         <input type="text" class="form-control" id="${id}" ${disabled} placeholder="${placeholder}" aria-label="NickName" aria-describedby="input_nickname">
       </div>
     `;
