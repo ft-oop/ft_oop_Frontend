450ebad7265aed2acfd2f9ff70c3b628c6089c84
zinnnn37 <zinnnn37@gmail.com>
2024-02-22 15:29:28 +0900

    feat : Login.js axios -> fetch

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -10,6 +10,8 @@ function Login($container) {
   };
 
   this.render = () => {
+    handleCode();
+
     this.$container.innerHTML = `
     <div class="flex flex-col justify-center items-center h-full">
       <img
@@ -22,7 +24,13 @@ function Login($container) {
         class="flex justify-center items-center text-2xl font-semibold hover:text-3xl hover:font-bold text-purple-400 w-[200px] h-[200px] bg-cover cursor-pointer"
         style="background-image: url(/bubble.png)"
       >
-        Login
+        <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-8e89d15b93c6c2cbc2a27bfce7e005898123d9102d922b7e04028cb238fc2d1b&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Flogin&response_type=code"
+          class="w-full h-full justify-center hover:text-3xl hover:text-purple-400"
+        >
+          <span class="w-full h-full rounded-full flex items-center justify-center align-middle">
+            Login
+          </span>
+        </a>
       </div>
     <div class="-z-10">
       <img
@@ -46,13 +54,39 @@ function Login($container) {
   };
   this.init = () => {
     this.render();
-    $('#loginBtn').addEventListener('click', (e) => {
-      const targetURL = window.location.href.replace(BASE_URL, '');
-      navigate(targetURL);
-    });
   };
 
   this.init();
+}
+
+async function handleCode() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const code = urlParams.get('code');
+
+  if (!code) return;
+
+  const data = {
+    code,
+  };
+
+  const response = await fetch(`${BASE_URL}/oauth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    console.log('로그인에 실패했습니다.');
+    return;
+  }
+
+  const result = response.json();
+  console.log('res: ', result);
+
+  navigate('/');
 }
 
 export default Login;
