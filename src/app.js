cdcd94eea426173f207efc2eac269f749d388df8
oio337a <oio337a@gmail.com>
2024-02-22 16:39:46 +0900

    chore : 소켓 연결 해제

--- src/app.js
+++ src/app.js
@@ -4,24 +4,8 @@ function App($container) {
   this.$container = $container;
 
   const init = () => {
-    socketConnect();
+    // const socket = new WebSocket('ws://localhost:3000');
     new Router($container);
-  };
-
-  const socketConnect = () => {
-    const socket = new WebSocket('ws://localhost:3000');
-
-    // 연결 성공 시 호출
-    socket.onopen = () => {
-      console.log('Connected to the server');
-    };
-
-    // 메시지 수신 시 호출
-    socket.onmessage = (event) => {
-      console.log('Message:', event.data);
-    };
-
-    socket.send('Hello, Server');
   };
 
   init();
