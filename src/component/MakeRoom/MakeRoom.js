7036a8ba71b8c921aa91fb7d017ff3af6bd5029a
zinnnn37 <zinnnn37@gmail.com>
2024-02-22 16:52:35 +0900

    feat : 숫자 외의 문자 입력 시 빨간 테두리 생성 -> 경고메시지 추가해야 함

--- src/component/MakeRoom/MakeRoom.js
+++ src/component/MakeRoom/MakeRoom.js
@@ -11,7 +11,10 @@ export default class MakeRoom extends Component {
           <span class='text-2xl font-bold'>방 만들기</span>
         </div>
         <div class='flex flex-col justify-center items-center w-2/3 flex-1 space-y-8 drop-shadow'>
-        ${bootstrapInput('이름', '회원님의 방', 'roomName')}
+        <div class="input-group">
+          <span class="input-group-text w-20 flex justify-center font-medium rounded-r-none">이름</span>
+          <input type="text" class="form-control" id="roomName" placeholder="회원님의 방" aria-label="NickName" aria-describedby="input_nickname">
+        </div>
         <div class="input-group w-full flex ">
           <label class="input-group-text w-20 flex justify-center font-medium rounded-r-none" for="inputGroupSelect01">타입</label>
           <select class="form-select flex-1 border px-[10px]" id="inputGroupSelect01" aria-label="Default select example">
@@ -28,7 +31,10 @@ export default class MakeRoom extends Component {
             <option value="2">8명</option>
           </select>
         </div>
-        ${bootstrapInput('비밀번호', '비밀방 원하면 입력해', 'c')}
+        <div class="input-group">
+          <span class="input-group-text w-20 flex justify-center font-medium rounded-r-none">비밀번호</span>
+          <input type="text" pattern="[0-9]+" class="form-control invalid:border-red-500" id="c" placeholder="비밀방 원하면 입력해" aria-label="NickName" aria-describedby="input_nickname">
+        </div>
         </div>
         <div class='mb-10 space-x-8'>
           <button type="button" id='cancelBtn' class="btn btn-secondary bg-gray-500 px-5 py-2">취소</button>
