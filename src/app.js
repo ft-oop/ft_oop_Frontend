049141c5848ae5c71745afc8e74522367e1cc3a4
oio337a <oio337a@gmail.com>
2024-02-22 15:24:10 +0900

    feat : socket

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
-import Router from "./router";
+import Router from './router';
 
 function App($container) {
   this.$container = $container;
 
   const init = () => {
+    socketConnect();
     new Router($container);
+  };
+
+  const socketConnect = () => {
+    const socket = new WebSocket('ws://localhost:3000');
+
+    // 연결 성공 시 호출
+    socket.onopen = () => {
+      console.log('Connected to the server');
+    };
+
+    // 메시지 수신 시 호출
+    socket.onmessage = (event) => {
+      console.log('Message:', event.data);
+    };
+
+    socket.send('Hello, Server');
   };
 
   init();
