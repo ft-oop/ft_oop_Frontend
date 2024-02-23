91c864538d06e57109ddfff61762a478933aef97
zinnnn37 <zinnnn37@gmail.com>
2024-02-23 15:47:21 +0900

    fix : 숫자만 입력할 수 있게 해 뒀는데 제출 시 새로고침 됨.. 수정 필요

--- src/component/MakeRoom/MakeRoom.js
+++ src/component/MakeRoom/MakeRoom.js
@@ -5,6 +5,7 @@ import { $ } from '../../utils/querySelector';
 export default class MakeRoom extends Component {
   template() {
     return `
+      <form id="makeRoomForm" method="post">
       <div class='bg-white w-[500px] h-[600px] rounded-xl shadow-lg flex flex-col justify-between items-center'>
         <div class='ml-2 mt-2 self-start'>
           <span class='text-5xl'>🏡</span>
@@ -13,11 +14,11 @@ export default class MakeRoom extends Component {
         <div class='flex flex-col justify-center items-center w-2/3 flex-1 space-y-8 drop-shadow'>
         <div class="input-group">
           <span class="input-group-text w-20 flex justify-center font-medium rounded-r-none">이름</span>
-          <input type="text" class="form-control" id="roomName" placeholder="회원님의 방" aria-label="NickName" aria-describedby="input_nickname">
+          <input type="text" class="form-control" id="roomName" placeholder="회원님의 방" aria-label="NickName" aria-describedby="input_nickname" required>
         </div>
         <div class="input-group w-full flex ">
           <label class="input-group-text w-20 flex justify-center font-medium rounded-r-none" for="inputGroupSelect01">타입</label>
-          <select class="form-select flex-1 border px-[10px]" id="inputGroupSelect01" aria-label="Default select example">
+          <select class="form-select flex-1 border px-[10px]" id="inputGroupSelect01" aria-label="Default select example" required>
             <option selected></option>
             <option value="1">1:1 대전</option>
             <option value="2">토너먼트</option>
@@ -25,15 +26,20 @@ export default class MakeRoom extends Component {
         </div>
         <div class="input-group w-full flex">
           <label class="input-group-text w-20 flex justify-center font-medium rounded-r-none" for="inputGroupSelect01">인원수</label>
-          <select class="form-select flex-1 border px-[10px]" id="inputGroupSelect02">
+          <select class="form-select flex-1 border px-[10px]" id="inputGroupSelect02" required>
             <option selected></option>
             <option value="1">4명</option>
             <option value="2">8명</option>
           </select>
         </div>
-        <div class="input-group">
-          <span class="input-group-text w-20 flex justify-center font-medium rounded-r-none">비밀번호</span>
-          <input type="text" pattern="[0-9]+" class="form-control invalid:border-red-500" id="c" placeholder="비밀방 원하면 입력해" aria-label="NickName" aria-describedby="input_nickname">
+        <div class="w-full flex flex-col">
+          <div class="input-group">
+            <span class="input-group-text w-20 flex justify-center font-medium rounded-r-none">비밀번호</span>
+            <input id="passwordInputBox" type="text" pattern="[0-9]+" class="form-control invalid:border-red-500" id="c" placeholder="비밀방 원하면 입력해" aria-label="NickName" aria-describedby="input_nickname">
+          </div>
+          <span class="mt-[5px] ml-20 flex text-sm">
+            비밀번호는 숫자로만 입력해주세요.
+          </span>
         </div>
         </div>
         <div class='mb-10 space-x-8'>
@@ -41,6 +47,7 @@ export default class MakeRoom extends Component {
           <button type="submit" id='confirmBtn' class="btn btn-primary bg-blue-500 px-5 py-2">확인</button>
         </div>
       </div>
+      </form>
     `;
   }
 
@@ -60,5 +67,29 @@ export default class MakeRoom extends Component {
         $('#inputGroupSelect02').removeAttribute('disabled', false);
       }
     });
+
+    this.addEvent('submit', '#makeRoomForm', (e) => {
+      // 정보 검증
+      // this.checkValid(e);
+
+      console.log('room has been made');
+      // 방 정보 전송
+
+      this.$target.remove();
+      e.preventDefault();
+    });
+  }
+
+  checkValid(e) {
+    // 왜 새로고침 되지..
+    const roomName = $('#roomName').value;
+    const roomType = $('#inputGroupSelect01').value;
+    const roomSize = $('#inputGroupSelect02').value;
+    const roomPassword = $('#c').value;
+
+    console.log('roomName: ', roomName);
+    console.log('roomType: ', roomType);
+    console.log('roomSize: ', roomSize);
+    console.log('roomPassword: ', roomPassword);
   }
 }
