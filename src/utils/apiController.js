13faf3888838d98d34995c539daf6967e279aa3b
oio337a <oio337a@gmail.com>
2024-03-03 14:35:46 +0900

    chore : 비동기 제거

--- src/utils/apiController.js
+++ src/utils/apiController.js
@@ -28,7 +28,7 @@ axios.interceptors.request.use(
 
 // 응답 인터셉터 추가하기
 axios.interceptors.response.use(
-  async function (response) {
+  function (response) {
     // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
     // 응답 데이터가 있는 작업 수행
 
