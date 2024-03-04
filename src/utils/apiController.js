c42d751bac39709007f9a4ec2c383a275403f6fc
oio337a <oio337a@gmail.com>
2024-03-04 22:21:07 +0900

    test : 토큰 재발급 테스트

--- src/utils/apiController.js
+++ src/utils/apiController.js
@@ -64,12 +64,10 @@ apiController.interceptors.response.use(
       // const accessToken = await reissueToken();
 
       const { data } = await apiController(reissueConfig);
-      console.log('data', data);
-      const { accessToken } = data;
+      const { access } = data;
+      localStorage.setItem('accessToken', access);
 
-      localStorage.setItem('accessToken', accessToken);
-
-      // return await apiController(config);
+      return await apiController(config);
     } else if (status === 403) {
       // 토큰 없이 main 진입
       navigate('/login');
