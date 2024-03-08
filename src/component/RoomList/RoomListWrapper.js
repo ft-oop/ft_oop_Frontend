a0f18791fe2322488972036a2027c84facadf1cf
oio337a <oio337a@gmail.com>
2024-03-08 16:51:44 +0900

    feat : grid로 변경

--- src/component/RoomList/RoomListWrapper.js
+++ src/component/RoomList/RoomListWrapper.js
@@ -6,12 +6,12 @@ import { navigate } from '../../utils/navigate';
 export default class RoomListWrapper extends Component {
   template() {
     return `
-      <div class='w-full h-[80%] flex flex-col justify-between items-center'>
+      <div class='w-full h-[80%] flex flex-col justify-start items-center'>
         <img id='goBack' src="/eva--arrow-back-fill.svg" alt="close" class='h-8 absolute top-6 left-6 rounded-full p-1 hover:shadow-md'/>
         <div class='text-3xl font-bold mb-4'>
           <span class='text-4xl mr-2'>🗒</span>방 목록
         </div>
-        <div id='room-list-detail' class='flex-1 w-full h-full px-6'></div>
+        <div id='room-list-detail' class='w-full px-6 grid grid-cols-2 gap-3'></div>
       </div>
     `;
   }
@@ -20,7 +20,29 @@ export default class RoomListWrapper extends Component {
     this.addEvent('click', '#goBack', (e) => {
       navigate('/');
     });
-
+    this.props = [
+      {
+        name: '방 이름 1',
+        type: 1,
+        max: 2,
+        enter: 1,
+        secret: false,
+      },
+      {
+        name: '방 이름 2',
+        type: 2,
+        max: 8,
+        enter: 2,
+        secret: true,
+      },
+      {
+        name: '방 이름 3',
+        type: 1,
+        max: 2,
+        enter: 1,
+        secret: false,
+      },
+    ];
     // 소켓으로 방 정보 받아오기.
     new RoomListDetail($('#room-list-detail'), this.props);
   }
