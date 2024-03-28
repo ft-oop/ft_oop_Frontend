346d5f47d258674bb7f2e1d3edd81a4c90cc9f09
oio337a <oio337a@gmail.com>
2024-03-28 22:17:07 +0900

    feat : socket url        정

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,7 @@ let socket = null;
 
 function initWebSocket() {
   if (!socket || socket.readyState !== WebSocket.OPEN) {
-    socket = new WebSocket('ws://localhost:8000/ws/');
+    socket = new WebSocket('ws://localhost:8000/ws/main/');
   }
 
   socket.onopen = () => {
